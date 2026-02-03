b698f28c9039967f14774f27bff52c7a9134d5dc
Fazil <fazil.gil1234@gmail.com>
2026-02-03 15:33:31 +0530

    practicing javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+// Arithmetic operators
+// let a = 10;
+// let b = 5;
+// console.log("value of a=", a, "& b= ", b);
+// console.log("a + b =", a + b);
+// console.log("b - a =", b - a);
+// console.log("b * a =", b * a);
+// console.log("b / a =", b / a);
+// console.log("b % a =", b % a);  
+// console.log("b ** a =", a ** b);
+
+//unary operators
+
+// let a = 10;
+// let b = 2;
+// console.log("a = ",a, "& b = ",b);
+// // a++;
+// console.log(a++);
+// console.log(a--);
+// console.log(--a);
+
+// Assignment operators
+// let a = 10;
+// let b = 5;
+// a+=9;
+// console.log(a);
+// a-=3;
+// console.log(a);
+// a*=2;
+// console.log(a);
+// a/=4;
+// console.log(a);
+// let a=15;
+// let b=15;
+// console.log("a>=b", a>=b);
+// let a= 10;
+// let b= 15;
+// let cond1= a>b;
+// let cond2= a<=b;
+// // console.log ("cond1 && cond2=", cond1 && cond2);
+//  console.log ("!(a>b)=", !(a>b));
+// let age= 18;
+// if (age >=18){console.log("You are eligible to vote");
+// }
+// if (age <=18){console.log("You are not eligible to vote");
+// }
+// let age=9;
+// if (age>=18){
+//     console.log("you are eligible to vote");
+// }
+// else{
+//     console.log("you are not eligible to vote");
+// }
+let num=10;
+if (num%2 ==0){
+    console.log("The number is even");
+}
+else{
+    console.log("The number is odd");
+}
